d2905d6bf91b6a13f042fcd4c2b70995125b0da1
Mubasshir08 <msr4212019@gmail.com>
2025-03-18 05:02:49 +0600

    update env

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+}));
 const port = process.env.PORT || 5000;
 const upload = multer({ dest: "uploads/" });
 
